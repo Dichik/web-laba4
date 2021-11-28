4636f1797e2bb7b253edc33a4c1bc228f897add6
Dima <omeluan.dima@gmail.com>
2021-11-28 09:02:29 +0200

    Menu options added

---
+++ src/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="container">
+
+            <h1 className="text-container" style={{paddingTop: "30%"}}>
+                Home
+            </h1>
+
+        </div>
+    );
+}
+
+export default Home;
